46a534002a97448ef450a3ca79d10078825866d8
Chris Eller <cteller@bsu.edu>
2019-11-07 13:13:22 -0500

    Create BuiltInSorter

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,23 @@
 import exporters.HTMLExporter;
 import exporters.NumberExporter;
+import providers.FromFileNumberProvider;
 import providers.NumberProvider;
 import providers.RandomNumberProvider;
 import sorters.BubbleSorter;
+import sorters.BuiltInSorter;
 import sorters.NumberSorter;
 
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        NumberProvider theProvider = new RandomNumberProvider(System.nanoTime());
+        NumberProvider theProvider = new FromFileNumberProvider();
 
         var theNumbers = theProvider.provideNumbers();
 
         System.out.println("BEFORE SORTING: [" + theNumbers.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
 
-        NumberSorter theSorter = new BubbleSorter();
+        NumberSorter theSorter = new BuiltInSorter();
         theSorter.sort(theNumbers);
 
         System.out.println("AFTER SORTING : [" + theNumbers.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
