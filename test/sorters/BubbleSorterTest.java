f9790546098205a650a019d6f35884c20e910318
Huseyin Ergin <mersinus@gmail.com>
2019-11-07 11:21:26 -0500

    Ready for in-class exercise.

--- test/sorters/BubbleSorterTest.java
+++ test/sorters/BubbleSorterTest.java
@@ -6,12 +6,12 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-class VeryBasicSorterTest {
+class BubbleSorterTest {
 
     @Test
     void sort() {
         var exampleList = Arrays.asList(4, 3, 2, 1);
-        new VeryBasicSorter().sort(exampleList);
+        new BubbleSorter().sort(exampleList);
 
         var sorted = Arrays.asList(1, 2, 3, 4);
         assertArrayEquals(sorted.toArray(), exampleList.toArray());
