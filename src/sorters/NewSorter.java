15825781d001e26316be6e497f755954337fff11
nabartling <nbart5495@gmail.com>
2019-11-07 15:49:01 -0500

    finish things

---
+++ src/sorters/NewSorter.java
@@ -0,0 +1,13 @@
+package sorters;
+
+import java.util.List;
+import java.util.*;
+
+public class NewSorter implements NumberSorter {
+
+    @Override
+    public void sort(List<Integer> items) {
+        Collections.sort(items);
+
+    }
+}
