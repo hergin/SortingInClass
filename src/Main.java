5ff62d61f439eb207899910e43ab109734fcd24b
Michael Gallegos <magallegos56@gmail.com>
2019-11-07 17:19:38 -0500

    Created ConsoleExporter, ConsoleProvider, and CollectionSorter.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
+import exporters.ConsoleExporter;
 import exporters.HTMLExporter;
 import exporters.NumberExporter;
+import providers.ConsoleProvider;
+import providers.FromFileNumberProvider;
 import providers.NumberProvider;
 import providers.RandomNumberProvider;
 import sorters.BubbleSorter;
+import sorters.CollectionsSorter;
 import sorters.NumberSorter;
 
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        NumberProvider theProvider = new RandomNumberProvider(System.nanoTime());
+        NumberProvider theProvider = new ConsoleProvider();
 
         var theNumbers = theProvider.provideNumbers();
 
         System.out.println("BEFORE SORTING: [" + theNumbers.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
 
-        NumberSorter theSorter = new BubbleSorter();
+        NumberSorter theSorter = new CollectionsSorter();
         theSorter.sort(theNumbers);
 
         System.out.println("AFTER SORTING : [" + theNumbers.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
 
-        NumberExporter theExporter = new HTMLExporter("numbersFormatted");
+        NumberExporter theExporter = new ConsoleExporter();
         theExporter.export(theNumbers);
     }
 }
