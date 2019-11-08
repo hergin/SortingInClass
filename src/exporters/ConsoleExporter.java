e8b2065ab8ad94a9eb2f7a72d92e40da2de98524
jaman1000 <javonelliott28@gmail.com>
2019-11-08 08:54:58 -0500

    OOP

---
+++ src/exporters/ConsoleExporter.java
@@ -0,0 +1,10 @@
+package exporters;
+
+import java.util.List;
+
+public class ConsoleExporter implements NumberExporter {
+    @Override
+    public void export(List<Integer> numbers) {
+    System.out.print(numbers);
+    }
+}
---
+++ src/sorters/NumberSort.java
@@ -0,0 +1,12 @@
+package sorters;
+
+import java.util.Collections;
+import java.util.List;
+
+public class NumberSort implements NumberSorter {
+    @Override
+    public void sort(List<Integer> items) {
+                Collections.sort(items);
+
+            }
+        }
