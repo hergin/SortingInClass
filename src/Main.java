eaf31b689af95b11cd58e21b2dc105e938e44795
illuminubby <illuminubby@gmail.com>
2019-11-08 17:02:44 -0500

    Added new sorter class 'BasicSorter' that uses the built-in sort() method for Collections Added Tester class for testing validity of 'BasicSorter'

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import exporters.HTMLExporter;
 import exporters.NumberExporter;
+import providers.FromFileNumberProvider;
 import providers.NumberProvider;
 import providers.RandomNumberProvider;
+import sorters.BasicSorter;
 import sorters.BubbleSorter;
 import sorters.NumberSorter;
 
@@ -9,13 +11,13 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        NumberProvider theProvider = new RandomNumberProvider(System.nanoTime());
+        NumberProvider theProvider = new FromFileNumberProvider();
 
         var theNumbers = theProvider.provideNumbers();
 
         System.out.println("BEFORE SORTING: [" + theNumbers.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
 
-        NumberSorter theSorter = new BubbleSorter();
+        NumberSorter theSorter = new BasicSorter();
         theSorter.sort(theNumbers);
 
         System.out.println("AFTER SORTING : [" + theNumbers.stream().map(Object::toString).collect(Collectors.joining(",")) + "]");
