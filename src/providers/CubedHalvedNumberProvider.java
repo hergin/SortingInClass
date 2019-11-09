e6f3abbe008dae4c2494cee65e1df9f939289cb3
chris <cakellum@bsu.edu>
2019-11-09 09:16:45 -0500

    done

--- src/providers/CubedHalvedNumberProvider.java
+++ src/providers/CubedHalvedNumberProvider.java
@@ -5,17 +5,25 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CubedHalvedNumberProvider implements NumberProvider {
+
+    private Scanner key;
+    private List<Integer> calculatedList;
+
     @Override
     public List<Integer> provideNumbers() {
-        List<Integer> calculatedList = new ArrayList<Integer>();
-        Scanner key = new Scanner(System.in);
+        this.input();
+        return calculatedList;
+    }
+
+    private void input(){
+        this.calculatedList = new ArrayList<>();
+        this.key = new Scanner(System.in);
         System.out.print("How many Numbers are you going to enter?\n");
         int amt = key.nextInt();
         for(int i=0; i<amt; i++){
             System.out.print("Number "+(i+1)+":\n");
             calculatedList.add(calculate(key.nextInt()));
         }
-        return calculatedList;
     }
 
     private int calculate(int nextInt) {
